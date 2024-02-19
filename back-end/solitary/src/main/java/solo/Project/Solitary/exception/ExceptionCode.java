28115aa5b9c376283df3abd02ee9a1ce38ed4711
hukabo <dlwlsdn153@gmail.com>
2024-02-19 23:45:23 +0900

    feat: create member exist exception

---
+++ back-end/solitary/src/main/java/solo/Project/Solitary/exception/ExceptionCode.java
@@ -0,0 +1,17 @@
+package solo.Project.Solitary.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ExceptionCode {
+    MEMBER_EXIST(404, "member is already exist"),
+    MEMBER_NOT_FOUND(404,"member not found");
+
+
+    private int status;
+    private String message;
+    ExceptionCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
