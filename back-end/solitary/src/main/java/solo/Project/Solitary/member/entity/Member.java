68c0f3bea742a56af4fd98b13ff696f026573bba
hukabo <dlwlsdn153@gmail.com>
2024-02-21 20:10:33 +0900

    feat: member엔티티

--- back-end/solitary/src/main/java/solo/Project/Solitary/member/entity/Member.java
+++ back-end/solitary/src/main/java/solo/Project/Solitary/member/entity/Member.java
@@ -1,10 +1,7 @@
 package solo.Project.Solitary.member.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -12,7 +9,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "member")
-@Getter
+@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
