b3ed277930ed0fb9373918f1362d391af9fa8b39
hukabo <dlwlsdn153@gmail.com>
2024-02-21 20:12:50 +0900

    member mapper

---
+++ back-end/solitary/src/main/java/solo/Project/Solitary/member/mapper/MemberMapper.java
@@ -0,0 +1,45 @@
+package solo.Project.Solitary.member.mapper;
+
+import org.mapstruct.Mapper;
+import solo.Project.Solitary.member.dto.MemberDto;
+import solo.Project.Solitary.member.entity.Member;
+
+import static solo.Project.Solitary.member.dto.MemberDto.*;
+
+@Mapper(componentModel = "spring")
+public interface MemberMapper {
+    default Member memberPostDtoToMember(MemberPostDto memberPostDto) {
+        if (memberPostDto == null) {
+            return null;
+        }
+
+        return Member.builder()
+                .email(memberPostDto.getEmail())
+                .memberName(memberPostDto.getMemberName())
+                .password(memberPostDto.getPassword())
+                .build();
+    }
+
+    default Member memberPatchDtoToMember(MemberPatchDto memberPatchDto) {
+
+        if (memberPatchDto == null) {
+            return null;
+        }
+
+        return Member.builder()
+                .memberId(memberPatchDto.getMemberId())
+                .memberName(memberPatchDto.getMemberName())
+                .password(memberPatchDto.getPassword())
+                .build();
+    }
+    default MemberResponseDto memberToResponseDto(Member member) {
+        if (member == null) {
+            return null;
+        }
+
+        return MemberResponseDto.builder()
+                .email(member.getEmail())
+                .memberName(member.getMemberName())
+                .build();
+    }
+}
