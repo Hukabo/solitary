324686c1b76faec459c86db0ec0aac69f142e9e5
hukabo <dlwlsdn153@gmail.com>
2024-02-23 21:38:38 +0900

    feat: add GlobalExceptionAdvice

---
+++ back-end/solitary/src/main/java/solo/Project/Solitary/exception/GlobalExceptionAdvice.java
@@ -0,0 +1,51 @@
+package solo.Project.Solitary.exception;
+
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import solo.Project.Solitary.response.ConstraintViolationError;
+import solo.Project.Solitary.response.ErrorResponse;
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException( // @Valid에서 발생
+            MethodArgumentNotValidException e) {
+        return ErrorResponse.of(e.getBindingResult());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleConstraintViolationException ( // JPA에서 데이터베이스 제약 조건 불충족시 발생
+            ConstraintViolationException e) {
+        return ErrorResponse.of(e.getConstraintViolations());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public ErrorResponse handleHttpRequestMethodNotSupportedException(
+            HttpRequestMethodNotSupportedException e) {
+        return ErrorResponse.of(HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(Exception e) {
+        log.error("# handle Exception", e);
+
+        return ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    public ErrorResponse handleBusinessLogicException(BusinessLogicException e) {
+        log.error("# handle Exception", e);
+
+        return ErrorResponse.of(e.getExceptionCode());
+    }
+}
