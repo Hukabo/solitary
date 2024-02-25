fca668f17d39940d254d5f46cf1451c8c1496e4f
hukabo <dlwlsdn153@gmail.com>
2024-02-25 23:09:56 +0900

    test : MemberRepositoryTest

--- back-end/solitary/src/test/java/solo/Project/Solitary/repository/MemberRepositoryTest.java
+++ back-end/solitary/src/test/java/solo/Project/Solitary/repository/MemberRepositoryTest.java
@@ -1,31 +1,29 @@
 package solo.Project.Solitary.repository;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import solo.Project.Solitary.entity.Member;
+import solo.Project.Solitary.member.entity.Member;
+import solo.Project.Solitary.member.repository.MemberRepository;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class MemberRepositoryTest {
     @Autowired
     private MemberRepository memberRepository;
 
     @Test
     @DisplayName("회원 등록 테스트")
-    public void postMember() {
+    public void saveMember() {
         //given
-        final Member member = Member.builder()
-                .email("dlwlsdn153@gmail.com")
-                .password("ABC123")
-                .memberName("이진우")
-                .memberRating(Member.MemberRating.BRONZE)
-                .build();
+        final Member member = buildMember();
 
         //when
         final Member save = memberRepository.save(member);
@@ -34,7 +32,6 @@ public class MemberRepositoryTest {
         assertThat(save.getMemberId()).isNotNull();
         assertThat(save.getEmail()).isEqualTo("dlwlsdn153@gmail.com");
         assertThat(save.getMemberName()).isEqualTo("이진우");
-        assertThat(save.getMemberRating()).isEqualTo(Member.MemberRating.BRONZE);
         assertThat(save.getPassword()).isEqualTo("ABC123");
     }
 
@@ -42,22 +39,41 @@ public class MemberRepositoryTest {
     @DisplayName("회원 조회 테스트")
     public void checkMember() {
         //given
-        final Member member = Member.builder()
-                .email("dlwlsdn153@gmail.com")
-                .password("ABC123")
-                .memberName("이진우")
-                .memberRating(Member.MemberRating.BRONZE)
-                .build();
+        final Member member = buildMember();
+
         memberRepository.save(member);
 
         //when
-        Member find = memberRepository.findByEmail(member.getEmail());
+        Optional<Member> optionalMember = memberRepository.findByEmail(member.getEmail());
+        Member find = optionalMember.get();
 
         //then
         assertThat(find.getMemberId()).isNotNull();
         assertThat(find.getEmail()).isEqualTo("dlwlsdn153@gmail.com");
         assertThat(find.getMemberName()).isEqualTo("이진우");
-        assertThat(find.getMemberRating()).isEqualTo(Member.MemberRating.BRONZE);
         assertThat(find.getPassword()).isEqualTo("ABC123");
+    }
+
+    @Test
+    @DisplayName("회원 탈퇴 테스트")
+    public void deleteMember() {
+        //given
+        Member member = buildMember();
+        memberRepository.save(member);
+
+        //when
+        memberRepository.deleteById(member.getMemberId());
+        Optional<Member> memberReturn = memberRepository.findById(member.getMemberId());
+
+        //then
+        assertThat(memberReturn).isEmpty();
+    }
+
+    public Member buildMember() {
+        return Member.builder()
+                .email("dlwlsdn153@gmail.com")
+                .password("ABC123")
+                .memberName("이진우")
+                .build();
     }
 }
