eaf9ee081a9d7deaa9b6d4c2619fdc161614b4ff
hukabo <dlwlsdn153@gmail.com>
2024-03-07 00:08:35 +0900

    feat : jwt 검증 필터 구현

--- back-end/solitary/src/main/java/solo/Project/Solitary/config/JwtAuthenticationFilter.java
+++ back-end/solitary/src/main/java/solo/Project/Solitary/config/JwtAuthenticationFilter.java
@@ -5,17 +5,23 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
@@ -31,6 +37,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             return;
         }
         jwt = authHeader.substring(7);
-        userEmail = jwtService.extractUserEmail(jwt);// jwt claims에서 email추출
+        userEmail = jwtService.extractUserEmail(jwt); // jwt claims에서 email추출
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) { // 로그인 상태가 아닌 경우
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail); // 토큰에서 꺼내온 이메일로 db조회 후 UserDetails 생성(회원이 없을 경우 UserNotFound 에러 throws)
+            if (jwtService.isTokenValid(jwt, userDetails)) { // 유효한 토큰이라면
+                // 인증 토큰을 생성하여 SecurityContextHolder에 set up
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request)); // 사용자의 세부 정보 저장 (ip 주소, 세션 id, 요청한 url 등)
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 }
