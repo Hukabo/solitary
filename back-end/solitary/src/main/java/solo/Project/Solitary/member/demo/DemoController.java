f20e1cec0861494b3052a1fb1bff42bf5e451993
hukabo <dlwlsdn153@gmail.com>
2024-03-12 23:24:24 +0900

    feat: add demo controller for security endpoint

---
+++ back-end/solitary/src/main/java/solo/Project/Solitary/member/demo/DemoController.java
@@ -0,0 +1,17 @@
+package solo.Project.Solitary.member.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> domoController() {
+        return ResponseEntity.ok("hello security endpoint");
+    }
+}
