cdf1e483f643d5da04e59898d254d725fe8346e4
hukabo <dlwlsdn153@gmail.com>
2024-04-06 04:19:49 +0900

    feat: PageResponse Dto 추가

---
+++ back-end/solitary/src/main/java/solo/Project/Solitary/response/PageResponse.java
@@ -0,0 +1,14 @@
+package solo.Project.Solitary.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResponse<T> {
+
+    int recordCount;
+    T response;
+}
