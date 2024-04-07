190343c58f24302e37b13d6ae249b1696365913e
hukabo <dlwlsdn153@gmail.com>
2024-04-07 10:21:37 +0900

    add: recipe not found 추가

--- back-end/solitary/src/main/java/solo/Project/Solitary/exception/ExceptionCode.java
+++ back-end/solitary/src/main/java/solo/Project/Solitary/exception/ExceptionCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum ExceptionCode {
     MEMBER_EXIST(404, "member is already exist"),
     MEMBER_NOT_FOUND(404,"member not found"),
-    IMAGE_NOT_FOUNT(404, "image not found");
+    IMAGE_NOT_FOUNT(404, "image not found"),
+    RECIPE_NOT_FOUND(404, "recipe not found");
 
 
     private int status;
