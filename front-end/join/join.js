fc9759a833fc91220c18e232d6a803c4d375ad59
hukabo <dlwlsdn153@gmail.com>
2024-04-07 10:36:22 +0900

    remove: fieldSet태그 삭제

--- front-end/join/join.js
+++ front-end/join/join.js
@@ -1,12 +1,11 @@
-// 회원가입 페이지에서 회원가입시, 회원 데이터를 서버로 전송해서 db에 저장한 다음, 성공했을 경우, 홈 화면으로 리다이렉팅 혹은 성공 페이지,,,
-const memberName = document.getElementById('name');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
 
 
 document.getElementById("signupForm").addEventListener('submit', (event) => {
     event.preventDefault();
     
+    const memberName = document.getElementById('name');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
 
     const requestData = {
         memberName: memberName.value,
@@ -32,7 +31,7 @@ document.getElementById("signupForm").addEventListener('submit', (event) => {
             alert("이미 존재하는 회원입니다.");
             return;
         }
-        alert(`${data.memberName}님 환영합니다!`);
+        alert(`회원가입 완료!`);
         window.location.href="../home/home.html"
     })
     .catch(error => {
