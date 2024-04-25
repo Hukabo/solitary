22cdcafb924fa41e6963d1d7653bbc8b57c2379c
hukabo <dlwlsdn153@gmail.com>
2024-04-25 11:41:01 +0900

    modify: member exception message

--- back/solitary/src/main/java/solo/Project/Solitary/exception/ExceptionCode.java
+++ back/solitary/src/main/java/solo/Project/Solitary/exception/ExceptionCode.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum ExceptionCode {
-    MEMBER_EXIST(404, "member is already exist"),
+    MEMBER_EXIST(404, "member already exist"),
     MEMBER_NOT_FOUND(404,"member not found"),
     IMAGE_NOT_FOUNT(404, "image not found"),
     RECIPE_NOT_FOUND(404, "recipe not found");
